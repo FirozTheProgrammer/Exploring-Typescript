86a201dc4f945fb5c71576b94608c668aa9d2f34
FirozTheProgrammer <firozhasan1542@gmail.com>
2024-05-06 11:52:54 +0600

    typeScript function

--- module1/dist/1.5 typescript object.js
+++ module1/dist/1.5 typescript object.js
@@ -2,6 +2,7 @@
 // Reference type --> Object
 const user = {
     company: "Programming Hero",
+    companyType: "Information Technology",
     firstName: "Firoz",
     middleName: "AL",
     lastName: "Hasan",
---
+++ module1/src/1.6 function.ts
@@ -0,0 +1,31 @@
+// Learning Funciton
+// Normal Function
+// Arrow function
+
+function add(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+add(2, 3);
+
+const addArrow = (num1: number, num2: number): number => {
+  return num1 + num2;
+};
+
+addArrow(5, 5);
+
+// object --> funcion === method
+
+const poorUser = {
+  name: "Firoz",
+  balance: 0,
+  addBallan(balance: number): string {
+    return `My new balance is : ${this.balance + balance}`;
+  },
+};
+
+// Map Function
+const arr: number[] = [3, 5, 7, 4, 9];
+
+const newArr: number[] = arr.map((elem: number): number => elem * elem);
+console.log(newArr);
